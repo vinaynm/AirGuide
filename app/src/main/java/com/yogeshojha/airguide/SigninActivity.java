b97ac9ac117d438b64538e3221704ab490fefe1e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-10-07 17:30:42 +0530

    Signin Done

---
+++ app/src/main/java/com/yogeshojha/airguide/SigninActivity.java
@@ -0,0 +1,13 @@
+package com.yogeshojha.airguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
